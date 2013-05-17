4ba93835ca0a768426f72aaf80e364990729073b
kgullikson <kgullikson@d6b50321-07e9-4bfa-8fc4-a7c1117b0175>
2013-05-17 20:11:54 +0000

    Added function for searching for stars (called Search.py)

--- PlotHETfits.py
+++ PlotHETfits.py
@@ -2,24 +2,28 @@ import pyfits
 import matplotlib.pyplot as plt
 import sys
 import FitsUtils
+import FindContinuum
 
 
 if __name__ == "__main__":
   fileList = []
   tellurics = False
   for arg in sys.argv[1:]:
-    if "tell" in arg:
+    if "tellcorr" in arg:
       tellurics = True
     else:
       fileList.append(arg)
 
   for fname in fileList:
     orders = FitsUtils.MakeXYpoints(fname, extensions=True, x="wavelength", y="flux", cont="continuum", errors="error")
+    print fname, len(orders)
     if tellurics:
       model = FitsUtils.MakeXYpoints(fname, extensions=True, x="wavelength", y="model")
     for i, order in enumerate(orders):
+      order.cont = FindContinuum.Continuum(order.x, order.y)
       if tellurics:
-        plt.plot(order.x, order.y/order.cont/model[i].y)
+        plt.plot(order.x, order.y/order.cont, 'k-')
+        plt.plot(order.x, model[i].y, 'r-')
       else:
         plt.plot(order.x, order.y/order.cont)
       
