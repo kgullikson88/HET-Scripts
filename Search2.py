ba682bf1ae0af7d1c8fb138310d68343869f44a0
kgullikson <kgullikson@d6b50321-07e9-4bfa-8fc4-a7c1117b0175>
2013-10-02 21:25:59 +0000

    Added a few features from the CHIRON Search to this one, and added the CHIRON PlotFits.py, which does the same thing as PlotHETFits.py but with more options.

--- Search2.py
+++ Search2.py
@@ -222,18 +222,32 @@ if __name__ == "__main__":
       for region in badregions:
         left = numpy.searchsorted(order.x, region[0])
         right = numpy.searchsorted(order.x, region[1])
-        order.x = numpy.delete(order.x, numpy.arange(left, right))
-        order.y = numpy.delete(order.y, numpy.arange(left, right))
-        order.cont = numpy.delete(order.cont, numpy.arange(left, right))
-        order.err = numpy.delete(order.err, numpy.arange(left, right))
+        if left == 0 or right == order.size():
+          order.x = numpy.delete(order.x, numpy.arange(left, right))
+          order.y = numpy.delete(order.y, numpy.arange(left, right))
+          order.cont = numpy.delete(order.cont, numpy.arange(left, right))
+          order.err = numpy.delete(order.err, numpy.arange(left, right))
+        else:
+          print "Warning! Bad region covers the middle of order %i" %i
+          print "Interpolating rather than removing"
+          order.y[left:right] = order.cont[left:right]
+          order.err[left:right] = 9e9
+
 
       #Remove whole order if it is too small
-      if order.x.size > 10:
-        order.cont = FindContinuum.Continuum(order.x, order.y, lowreject=3, highreject=3)
-        orders[numorders -1 -i] = order.copy()
+      remove = False
+      if order.x.size <= 1:
+        remove = True
       else:
+        velrange = 3e5 * (numpy.median(order.x) - order.x[0]) / numpy.median(order.x)
+        if velrange <= 1050.0:
+          remove = True
+      if remove:
         print "Removing order %i" %(numorders - 1 - i)
         orders.pop(numorders - 1 - i)
+      else:
+        order.cont = FittingUtilities.Continuum(order.x, order.y, lowreject=3, highreject=3)
+        orders[numorders -1 -i] = order.copy()
 
     """
     for i, order in enumerate(orders):
