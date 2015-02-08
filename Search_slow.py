6e589cf208393d8aba3b3543ca0032cd6f51706e
kgullikson88 <kevin.gullikson@gmail.com>
2015-02-08 09:56:54 -0600

    Reformatted with pycharm

--- Search_slow.py
+++ Search_slow.py
@@ -15,7 +15,7 @@ badregions = [[567.5, 575.5],
               [759, 9e9],
               # [655, 657],  # H alpha
               # [485, 487],  #H beta
-              #[433, 435],  #H gamma
+              # [433, 435],  #H gamma
               #[409, 411],  #H delta
               #[396, 398],  #H epsilon
               #[388, 390],  #H zeta
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     for arg in sys.argv[1:]:
         if 1:
             fileList.append(arg)
-    prim_vsini = [100.0]*len(fileList)
+    prim_vsini = [100.0] * len(fileList)
 
     # Get the primary star vsini values
     prim_vsini = []
@@ -45,30 +45,29 @@ if __name__ == '__main__':
     vsini_dict = {}
     for fname in fileList:
         root = fname.split('/')[-1][:9]
-	if root in vsini_dict:
-	    prim_vsini.append(vsini_dict[root])
+        if root in vsini_dict:
+            prim_vsini.append(vsini_dict[root])
         else:
             header = fits.getheader(fname)
             star = header['OBJECT1']
-	    print fname, star
+            print fname, star
             v = vsini.loc[vsini.Identifier.str.strip() == star]['vsini(km/s)'].values[0]
             prim_vsini.append(float(v) * 0.8)
             vsini_dict[root] = float(v) * 0.8
     for fname, vsini in zip(fileList, prim_vsini):
         print fname, vsini
-    
+
     GenericSearch.slow_companion_search(fileList, prim_vsini,
                                         hdf5_file='/media/ExtraSpace/PhoenixGrid/HRS_Grid.hdf5',
                                         extensions=True,
-					resolution=None,
-					trimsize=trimsize,
-					modeldir=modeldir,
-					badregions=badregions,
-					metal_values=(0.0,),
-					vsini_values=(1, 5.0, 10.0, 15.0),
-					#Tvalues=range(4100, 6900, 100),
-					observatory='McDonald',
-					debug=False,
-					vbary_correct=False,
-					addmode='simple')
+                                        resolution=None,
+                                        trimsize=trimsize,
+                                        modeldir=modeldir,
+                                        badregions=badregions,
+                                        metal_values=(0.0,),
+                                        vsini_values=(1, 5.0, 10.0, 15.0),  # Tvalues=range(4100, 6900, 100),
+                                        observatory='McDonald',
+                                        debug=False,
+                                        vbary_correct=False,
+                                        addmode='simple')
 
